1224fb547f4acb2ccc1236316f429e5b98925eb4
panosale <panosalevr@hotmail.gr>
2024-03-27 10:29:20 +0200

    Update Askisi3.asm

--- Askisi3.asm
+++ Askisi3.asm
@@ -6,9 +6,10 @@ kodikas segment
         mov ax, dedomena
         mov ds, ax    
         
-        mov si, 0           ; Initialize index
+        mov si, 0       ; Initialize index
+        mov cx, 40      ; Initialize CX index for loop
         
-        lea dx, prompt_msg  ; Print message, standard instructions
+        lea dx, PROMPT_MSG  ; Print message, standard instructions
         mov ah, 9           ; Print message, standard instructions
         int 21h             ; Print message, standard instructions
         
@@ -18,7 +19,7 @@ kodikas segment
         mov ah, 1h          ; Ask for a character, standard instructions
         int 21h             ; Ask for a character, standard instructions
         
-        cmp al, dh
+        cmp al, 13
         je enter_pressed
 
         cmp al, ' '         ; Compare given character to space
@@ -34,7 +35,7 @@ kodikas segment
         jbe save_to_buffer
 
         cmp al, 'a'
-        jb start_loop
+        jb ask_for_text
         
         cmp al, 'z'
         jbe save_to_buffer
@@ -42,38 +43,44 @@ kodikas segment
     save_to_buffer:
         mov BUFFER[si], al
         inc si
-        jmp ask_for_text
+        cmp si, 40
+;        je emfanisi ;;; den xreiazetai
+        loop start_loop
         
-    gave_wrong_entry:
+    enter_pressed:
+        cmp si, 0
+        je no_text_given
+        
+    emfanisi:
+        lea dx, MSG_CONVERTED_TEXT  ; Print message, standard instructions
+        mov ah, 9h                  ; Print message, standard instructions
+        int 21h                     ; Print message, standard instructions
+        jmp exit                    
+        
+    no_text_given:
+        lea dx, MSG_NO_TEXT         ; Print message, standard instructions
+        mov ah, 9h                  ; Print message, standard instructions
+        int 21h                     ; Print message, standard instructions
+        jmp exit
+                            
+;    gave_wrong_entry:
 ;        lea dx, wrong_entry ; Print message, standard instructions
 ;        mov ah, 9h          ; Print message, standard instructions
 ;        int 21h             ; Print message, standard instructions
 ;        jmp ask_for_number  ; Ask for a new correct number
 
-    emfanisi:
-        lea dx, msg_converted_text  ; Print message, standard instructions
-        mov ah, 9h                  ; Print message, standard instructions
-        int 21h                     ; Print message, standard instructions
-        jmp exit                    
-        
-    enter_pressed:
-        lea dx, msg_no_text         ; Print message, standard instructions
-        mov ah, 9h                  ; Print message, standard instructions
-        int 21h                     ; Print message, standard instructions
-        jmp exit                    
-
-    artios:
+    ;artios:
 ;        lea dx, msg_artios  ; Print message, standard instructions
 ;        mov ah, 9h          ; Print message, standard instructions
 ;        int 21h             ; Print message, standard instructions
 ;        jmp continue_loop
 
-    perittos:
+;    perittos:
 ;        lea dx, msg_perittos; Print message, standard instructions
 ;        mov ah, 9h          ; Print message, standard instructions
 ;        int 21h             ; Print message, standard instructions
         
-    continue_loop:      ; do ... while loop
+;    continue_loop:      ; do ... while loop
 ;        inc cl              ; Increase number of tries
 ;        cmp cl, max_tries   ; Compare number of tries
 ;        jb start_loop       ; Continue loop if tries are below max_tries
@@ -84,10 +91,12 @@ kodikas segment
 
 kodikas ends
 
-dedomena segment               
-    BUFFER db 40 dup(0)
-    prompt_msg db "Eisagete keimeno (A-Z, a-z,teleia, space): $"
-    msg_converted_text db 10, 13, "To keimeno meta tin metatropi: $"
-    msg_no_text db "Den dosate kanena keimeno.", 10, 13, "$"
+dedomena segment     
+    MAX_BUFFER_SIZE db 40          
+    BUFFER db MAX_BUFFER_SIZE dup(0)
+    
+    PROMPT_MSG db "Eisagete keimeno (A-Z, a-z,teleia, space): $"
+    MSG_CONVERTED_TEXT db 10, 13, "To keimeno meta tin metatropi: $"
+    MSG_NO_TEXT db 10, 13, "Den dosate kanena keimeno.$"
 dedomena ends
 end start
