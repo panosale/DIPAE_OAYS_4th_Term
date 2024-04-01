6bd908d67b19960ca571fdd2f138ddce0591dc2a
panosale <62485609+panosale@users.noreply.github.com>
2024-04-01 22:50:10 +0300

    Update Askisi3.asm

--- Askisi3.asm
+++ Askisi3.asm
@@ -6,51 +6,51 @@ kodikas segment
         mov ax, dedomena
         mov ds, ax    
         
-        mov si, 0           ; Initialize index
-        mov cx, 40          ; Initialize CX index for loop
+        mov si, 0                   ; Initialize index
+        mov cx, 40                  ; Initialize CX index for loop
         
-        lea dx, PROMPT_MSG  ; Print message, standard instructions
-        mov ah, 9           ; Print message, standard instructions
-        int 21h             ; Print message, standard instructions
+        lea dx, PROMPT_MSG          ; Print message, standard instructions
+        mov ah, 9                   ; Print message, standard instructions
+        int 21h                     ; Print message, standard instructions
         
-    start_loop:             ; Main prompt for character loop
+    start_loop:                     ; Main prompt for character loop
     
     ask_for_text:        
-        mov ah, 1h          ; Ask for a character, standard instructions
-        int 21h             ; Ask for a character, standard instructions
+        mov ah, 1h                  ; Ask for a character, standard instructions
+        int 21h                     ; Ask for a character, standard instructions
         
-        cmp al, 13          ; Compare given character with Enter
-        je enter_pressed    ; Finish the prompt. Exit loop
+        cmp al, 13                  ; Compare given character with Enter
+        je enter_pressed            ; Finish the prompt. Exit loop
 
-        cmp al, ' '         ; Compare given character with Space...
-        je save_to_buffer   ; ...and save it to BUFFER 
+        cmp al, ' '                 ; Compare given character with Space...
+        je save_to_buffer           ; ...and save it to BUFFER 
         
-        cmp al, '.'         ; Compare given character with Point(.)...
-        je save_to_buffer   ; ...and save it to BUFFER
+        cmp al, '.'                 ; Compare given character with Point(.)...
+        je save_to_buffer           ; ...and save it to BUFFER
         
-        cmp al, 'A'         ; Check if given character is below 'A'...
-        jb start_loop       ; ...and ask again
+        cmp al, 'A'                 ; Check if given character is below 'A'...
+        jb start_loop               ; ...and ask again
         
-        cmp al, 'Z'         ; Check if given character is below or equal to 'Z'...
-        jbe save_to_buffer  ; ...and save it to BUFFER
+        cmp al, 'Z'                 ; Check if given character is below or equal to 'Z'...
+        jbe save_to_buffer          ; ...and save it to BUFFER
 
-        cmp al, 'a'         ; Check if given character is below 'a'...
-        jb ask_for_text     ; ...and ask again
+        cmp al, 'a'                 ; Check if given character is below 'a'...
+        jb ask_for_text             ; ...and ask again
         
-        cmp al, 'z'         ; Check if given character is below or equal to 'z'...
-        jbe save_to_buffer  ; ...and save it to BUFFER
+        cmp al, 'z'                 ; Check if given character is below or equal to 'z'...
+        jbe save_to_buffer          ; ...and save it to BUFFER
         
-        jmp ask_for_text    ; None of above happened so continue to ask for characters            
+        jmp ask_for_text            ; None of above happened so continue to ask for characters            
         
     save_to_buffer:
-        mov BUFFER[si], al  ; Save given & checked character to position [si] of BUFFER
-        inc si              ; Increase BUFFER index [si] by 1
-        loop start_loop     ; Continue loop until 40 characters have been given (cx = 0)
+        mov BUFFER[si], al          ; Save given & checked character to position [si] of BUFFER
+        inc si                      ; Increase BUFFER index [si] by 1
+        loop start_loop             ; Continue loop until 40 characters have been given (cx = 0)
         
     enter_pressed:
-        cmp si, 0           ; Check if no text given...
-        je no_text_given    ; ...and show message
-                            ; ...else continue on showing converted text
+        cmp si, 0                   ; Check if no text given...
+        je no_text_given            ; ...and show message
+                                    ; ...else continue on showing converted text
     emfanisi:
         lea dx, MSG_CONVERTED_TEXT  ; Print message, standard instructions
         mov ah, 9h                  ; Print message, standard instructions
@@ -90,8 +90,8 @@ kodikas segment
         int 21h                     ; Print message, standard instructions
 
     exit:
-        mov ah, 4ch         ; Exit program, standard instructions
-        int 21h             ; Exit program, standard instructions
+        mov ah, 4ch                 ; Exit program, standard instructions
+        int 21h                     ; Exit program, standard instructions
 
 kodikas ends
 
